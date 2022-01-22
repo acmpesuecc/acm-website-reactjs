6fb39e75da42baaea0b47c3ee5d06776e402a108
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2022-01-22 11:48:08 +0530

    Add code for responsive design

--- src/components/pages/LangingPage/LandingPage.style.js
+++ src/components/pages/LangingPage/LandingPage.style.js
@@ -2,8 +2,16 @@ import styled from "styled-components";
 
 export const LandingPageContainer = styled.div`
   padding: 20px 10%;
+
+  @media (max-width: 950px) {
+    padding: 20px 10%;
+  }
 `;
 
 export const Title = styled.h1`
   color: ${({ theme }) => theme.secondaryFontColor};
+
+  @media (max-width: 950px) {
+    font-size: 1rem;
+  }
 `;
